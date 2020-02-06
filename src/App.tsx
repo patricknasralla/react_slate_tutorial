9dd5baf0bbbd34bd87b8c9f10f13c06baf38461a
Patrick Nasralla <patrick.nasralla@gmail.com>
2020-02-06 09:48:47 +0000

    02: Slate init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { createEditor, Node } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
-const App = () => {
-  return <div>Hello World!</div>;
+const App: React.FC = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  return (
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable />
+    </Slate>
+  );
 };
 
 export default App;
